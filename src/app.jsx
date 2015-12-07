9c2f5a69f9501d4ec65b7ddd02ea95aae62a166f
alexoviedo <alexoviedo999@gmail.com>
2015-12-07 10:26:17 -0500

    refactored for new app/router structer

--- src/app.jsx
+++ src/app.jsx
@@ -1,41 +1,7 @@
 var React = require('react');
-var ReactRouter = require('react-router');
-var HashHistory = require('react-router/lib/hashhistory');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-
-
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      test
-      {this.props.children}
-    </h1>
-  }
-});
-
-var Child1 = React.createClass({
-  render: function () {
-    return <h1>Im Child1</h1>
-  }
-});
-
-var Child2 = React.createClass({
-  render: function () {
-    return <h1>Im child 2</h1>
-  }
-});
-
-var routes = (
-  <Router history={new HashHistory}>
-    <Route path="/" component={Hello}>
-      <Route path="1" component={Child1} />
-      <Route path="2" component={Child2} />
-    </Route>
-  </Router>
-)
+var Routes = require('./routes');
 
 
 
-// var element = React.createElement(Hello, {});
-React.render(routes, document.querySelector('.container'));
+
+React.render(Routes, document.querySelector('.container'));
