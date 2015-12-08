a965303fdb7a612a68d602e5f92a962f2377db29
alexoviedo <alexoviedo999@gmail.com>
2015-12-07 22:57:46 -0500

    added imgur api data using reflux.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var Header = require('./header');
+var TopicList = require('./topic-list');
 
 module.exports = React.createClass({
 	render: function () {
 		return <div>
-				I am a header
-				{this.props.children}
-			</div>
+			<Header />
+			{this.content()}
+		</div>
+	},
+	content: function () {
+		if(this.props.children) {
+			return this.props.children
+		} else {
+			return <TopicList />
+		}
 	}
 });
